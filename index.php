4a49f7589c2aa357231a4bfb6f3393931e02a9cd
RyanSoares001 <143231421+RyanSoares001@users.noreply.github.com>
2024-08-11 03:48:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Ryan</title>
+</head>
+<body>
+    <header>
+        <div class="interface">
+            <div class="logo">
+                <a href="#">
+                    <img src="images/istockphoto-1201523607-612x612.jpg" alt="logo rods" width="50" height="50">
+                </a>
+            </div>
+            <nav class="menu-desktop">
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Especialidades</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Projetos</a></li>
+                </ul>
+            </nav>
+            <div class="btn-contato">
+                <a href="https://wa.me/5561991398517" target="_blank">
+                    <button>Contato</button>
+                </a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section class="topo-do-site">
+            <div class="perfil-container">
+                <img src="images/eu.jpg" alt="Imagem do perfil" class="perfil-imagem">
+                <div class="perfil-info">
+                    <h1>ALUNO DE ANÁLISE E DESENVOLVIMENTO PROCURANDO ESTÁGIO<span>.</span></h1>
+                    <p>Aluno de análise e desenvolvimento de sistemas apaixonado por tecnologia em geral, querendo sempre aprender mais e sempre disposto a ajudar.</p>
+                    <div class="btn-container">
+                        <a href="https://wa.me/5561991398517" target="_blank"></">
+                            <button>Entre em contato</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="sobre">
+            <div class="flex">
+                <div class="txt-sobre">
+                    <h2>Muito Prazer,<span> Sou Ryan Soares</span></h2>
+                    <p>Apaixonado desde sempre em tecnologia, desde pequeno no meio com edição de vídeo e fotos, jogos e montagem de computadores e limpeza de computadores. Atualmente estou no segundo semestre de análise e desenvolvimento de sistemas, já passei por vários cursos como, ciência da computação, nutrição e relações internacionais. Sempre estou buscando melhorar como pessoa e sempre estudando mais para poder ajudar no que eu conseguir.</p>
+                    <div class="bnt-social">
+                        <a href="https://www.instagram.com/_r.ods_/" target="_blank"><button><i class="bi bi-instagram"></i></button></a>    
+                        <i class="bi bi-chat-left-text-fill">61 991398517(Whatsapp)</i>           
+                     </div>
+                </div>
+                <div class="img-sobre">
+                </div>
+            </div>
+        </section>
+        <section class="especialidades">
+            <div class="interface">
+                <h2 class="titulo">Minhas <span>ESPECIALIDADES.</span></h2>
+                <div class="flex">
+                    <div class="especialidade-box">
+                        <i class="bi bi-code-square"></i>
+                        <h3>Web site</h3>
+                        <p>Minha maior especialidade é criar sites, usando HTML, PHP, JavaScript e CSS.</p>
+                    </div>
+                    <div class="especialidade-box">
+                        <i class="bi bi-camera-fill"></i>
+                        <h3>Edição de fotos e vídeos</h3>
+                        <p>Desde pequeno apaixonado por edição de fotos e vídeos, assim como tirar fotos e fazer vídeos.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="portfolio">
+            <div class="interface">
+                <h2 class="titulo"> MEU <span> PORTFOLIO.</span></h2>
+                <div class="btn-container">
+                    <a href="https://cogitatus.lovestoblog.com/?i=1" target="_blank"><button>Site que fiz</button></a>
+                    <a href="https://ryansoares001.github.io/projeto-hdo/" target="_blank"><button>Projeto HDO</button></a>
+                </div>
+                <div class="flex">
+                    <div class="img-port" style="background-image: url(images/site.png);">
+                        <div class="overlay">Cogitatus</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="formulario">
+            <div class="interface">
+  
+               
+</body>
+</html>
